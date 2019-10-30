fdc6fb62201cd60e32696d36eb1e2dd871c53403
raudbergetemil <raemil@student.chalmers.se>
2019-10-30 15:58:42 +0100

    added EKF update and prediction

--- state_estimator.py
+++ state_estimator.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-#%%
-# import keras
+
 import numpy as np
 
 """
 Prediction step of linear Kalman filter
 """
 def linear_kalman_prediction(x, P, A, Q):
-    return (A@x, A@P@A + Q)
+    return (A@x, A@P@A.T + Q)
 
 """
 Update step of linear Kalman filter
@@ -16,4 +15,19 @@ def linear_kalman_update(x, P, y, H, R):
     S = H@P@H.T + R
     K = P@H.T@np.linalg.inv(S)
     return (x + K@(y-H@x), P - K@H@P)
+
+"""
+Prediction step of EKF
+"""
+def ekf_prediction(x, P, f, dfdx, Q):
+    return (f(x), dfdx@P@dfdx.T + Q)
+
+"""
+Update step of EKF
+"""
+def ekf_update(x, P, y, h, dhdx, R):
+
+    S = dhdx@P@dhdx.T + R
+    K = P@dhdx.T@np.linalg.inv(S)
+    return (x + K@(y-h(x)), P - K@dhdx@P)
 
