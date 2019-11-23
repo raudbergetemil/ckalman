b9af13892a5d689615fbc51c93349b9e2e549e88
raudbergetemil <raemil@student.chalmers.se>
2019-11-23 21:00:06 +0100

    Added nonlinear motion model

--- simulator.py
+++ simulator.py
@@ -8,25 +8,28 @@ class Simulator:
         self.n = n # number of states
         self.o = o # number of measurements
         
-        self.x = np.ones(self.n) # current (hidden) state
+        self.x = 5*np.ones(self.n) # current (hidden) state
         self.y = np.zeros(self.m) # current observation
 
-        self.Ts = 0.01 # sampling time
+        self.Ts = 0.0000001 # sampling time
 
     
     def process_model(self, x, u): 
         """
         Maps current state to next state
 
-        TODO: 
-        - Add noise
-        - Create more interesting process models
         """
+        # # Linear model 
+        # A = 0.5*np.eye(self.n)
+        # B = np.eye(self.n, self.m)
         
-        A = 0.5*np.eye(self.n)
-        B = np.eye(self.n, self.m)
-        Q = np.eye(self.n, self.n)
-        return A@x + B@u + np.random.multivariate_normal(np.zeros((n,)),Q)
+        # Q = np.eye(self.n, self.n)
+        # return A@x + B@u + np.random.multivariate_normal(np.zeros((self.n,)),Q)
+
+        # Nonlinear model
+        # Discrete Volterra-Lotka predator-prey model
+        new_x = np.array([0.1*x[0]*(1-x[0]) - 0.7*x[0]*x[1], 0.1*x[0]*x[1]])
+        return new_x
 
     def measurement_model(self, x):
         """
@@ -39,7 +42,7 @@ class Simulator:
 
         H = np.eye(self.o, self.n)
         R = np.eye(self.o, self.o)
-        return H@x + np.random.multivariate_normal(np.zeros((m,)),R)
+        return H@x + np.random.multivariate_normal(np.zeros((self.m,)),R)
 
     def step(self, u):
         """
