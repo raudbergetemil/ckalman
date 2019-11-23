018943bffef0d3465d80d264217e59a8ff2ab73d
raudbergetemil <raemil@student.chalmers.se>
2019-11-23 17:37:43 +0100

    Changed names of models

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from state_estimator import *
-
+import scipy.io
 
 
 def create_trajectory(n,N,Q,f):
@@ -37,7 +37,7 @@ def test(pred_fcn, update_fcn):
     """ 
     Process model x_{k+1} = f(x_{k})
     """
-    def f(x):
+    def real_f(x):
         assert x.shape[0] == n, 'Number of rows in x does not match n'
 
     
@@ -46,20 +46,20 @@ def test(pred_fcn, update_fcn):
     """
     Measurement model y = h(x)
     """
-    def h(x):
+    def real_h(x):
         assert x.shape[0] == n, 'Number of rows in x does not match n'
 
         return H@x + np.random.multivariate_normal(np.zeros((m,)),R)
 
 
-    x = create_trajectory(n,N,Q,f)
-    y = create_measurements(x,h)
+    x = create_trajectory(n,N,Q,real_f)
+    y = create_measurements(x,real_h)
     x_hat = np.zeros((n,N))
     P_hat = np.zeros((N,n,n))
     P_hat[0,:,:] = Q
     for i in range(N-1):
-        x_hat[:,i], P_hat[i,:,:] = update_fcn(x_hat[:,i], P_hat[i,:,:], y[:,i], h, R)
-        x_hat[:,i+1], P_hat[i+1,:,:] = pred_fcn(x_hat[:,i], P_hat[i,:,:], f, Q)
+        x_hat[:,i], P_hat[i,:,:] = update_fcn(x_hat[:,i], P_hat[i,:,:], y[:,i], real_h, R)
+        x_hat[:,i+1], P_hat[i+1,:,:] = pred_fcn(x_hat[:,i], P_hat[i,:,:], real_f, Q)
     fig, axis = plt.subplots()
     axis.plot(x_hat[0,:])
     axis.plot(x[0,:])
@@ -68,4 +68,6 @@ def test(pred_fcn, update_fcn):
 pred_fcn = ckf_prediction
 update_fcn = ckf_update
 
-test(pred_fcn, update_fcn)
+#test(pred_fcn, update_fcn)
+data = scipy.io.loadmat('test.mat')
+print(data)
