75739b89ac459568d0c73dba42c6861b67061163
raudbergetemil <raemil@student.chalmers.se>
2019-11-23 19:01:52 +0100

    Using Simulator object in simulation

--- test.py
+++ test.py
@@ -5,16 +5,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 from state_estimator import *
 import scipy.io
+from simulator import Simulator
+from control import lqr_gain
 
 
-def create_trajectory(n,N,Q,f):
+def create_trajectory(n, N, Q, f, m=1):
     x0 = np.zeros(n)
     x0 = np.random.multivariate_normal(x0, Q)
     x = np.zeros((n,N))
+    u = np.zeros((m,N))
 
     for i in range(N-1):
-        x[:,i+1] = f(x[:,i])
-    return x
+        x[:,i+1] = f(x[:,i], u[:,i])
+    return x, u
 
 def create_measurements(x,h):
     N = x.shape[1]
@@ -29,19 +32,21 @@ def test(pred_fcn, update_fcn):
 
     n = 2
     m = 2
+    o = 2
     Q = np.eye(n)
     R = np.eye(m)
-    A = np.eye(n)
-    H = np.eye(m)
+    A = 0.9*np.eye(n)
+    B = np.eye(n)
+    H = np.eye(o)
     N = 100
     """ 
     Process model x_{k+1} = f(x_{k})
     """
-    def real_f(x):
+    def real_f(x, u):
         assert x.shape[0] == n, 'Number of rows in x does not match n'
 
     
-        return A@x + np.random.multivariate_normal(np.zeros((n,)),Q)
+        return A@x + B@u + np.random.multivariate_normal(np.zeros((n,)),Q)
 
     """
     Measurement model y = h(x)
@@ -52,22 +57,41 @@ def test(pred_fcn, update_fcn):
         return H@x + np.random.multivariate_normal(np.zeros((m,)),R)
 
 
-    x = create_trajectory(n,N,Q,real_f)
+    x,u = create_trajectory(n,N,Q,real_f, m)
     y = create_measurements(x,real_h)
     x_hat = np.zeros((n,N))
     P_hat = np.zeros((N,n,n))
     P_hat[0,:,:] = Q
+
+    sim = Simulator(n,m,o)
+    u = np.zeros((m,N))
+
+    # Calculate LQR gain
+    K = lqr_gain(A,B,Q,R)
+
     for i in range(N-1):
-        x_hat[:,i], P_hat[i,:,:] = update_fcn(x_hat[:,i], P_hat[i,:,:], y[:,i], real_h, R)
-        x_hat[:,i+1], P_hat[i+1,:,:] = pred_fcn(x_hat[:,i], P_hat[i,:,:], real_f, Q)
+        # Calculate control signal 
+        u[:,i] = -K@x_hat[:,i]
+        
+        # Take step in simulator environment and observe output
+        y[:,i+1] = sim.step(u[:,i])
+
+        # Predict from current estimate
+        x_hat[:,i+1], P_hat[i+1,:,:] = pred_fcn(x_hat[:,i], P_hat[i,:,:], A,B, u[:,i], Q)
+
+        # Update estimate 
+        x_hat[:,i+1], P_hat[i+1,:,:] = update_fcn(x_hat[:,i+1], P_hat[i+1,:,:], y[:,i+1], H, R)
+
+        # Save real current state
+        x[:,i+1] = sim.get_current_state()
+
     fig, axis = plt.subplots()
     axis.plot(x_hat[0,:])
     axis.plot(x[0,:])
     plt.show()
 
-pred_fcn = ckf_prediction
-update_fcn = ckf_update
+pred_fcn = linear_kalman_prediction
+update_fcn = linear_kalman_update
 
-#test(pred_fcn, update_fcn)
-data = scipy.io.loadmat('test.mat')
-print(data)
+test(pred_fcn, update_fcn)
+#data = scipy.io.loadmat('test.mat')
