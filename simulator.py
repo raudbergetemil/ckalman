26352e66b8748eb3041f83cb342606a7135b398d
raudbergetemil <raemil@student.chalmers.se>
2019-12-02 19:45:02 +0100

    Added RK4 integration method

--- simulator.py
+++ simulator.py
@@ -8,12 +8,26 @@ class Simulator:
         self.n = n # number of states
         self.o = o # number of measurements
         
-        self.x = 5*np.ones(self.n) # current (hidden) state
+        self.x = np.ones(self.n) # current (hidden) state
         self.y = np.zeros(self.m) # current observation
 
-        self.Ts = 0.0000001 # sampling time
-
+        self.Ts = 0.1 # sampling time
     
+    def RK4_next_step(self,x,u,f):
+        """
+        Returns the next state based on the Explicit Runge Kutta 4 method. 
+        Assumes that control signal is piecewise constant. Does not handle explicit 
+        time dependency.
+        """
+
+        # Calculate coefficients
+        k1 = self.Ts*f(x,u)
+        k2 = self.Ts*f(x + k1/2,u)
+        k3 = self.Ts*f(x + k2/2, u)
+        k4 = self.Ts*f(x + k3)
+
+        # Calculate and return value at next timestep
+        return x + 1/6*(k1+k2+k3+k4)
     def process_model(self, x, u): 
         """
         Maps current state to next state
@@ -27,9 +41,19 @@ class Simulator:
         # return A@x + B@u + np.random.multivariate_normal(np.zeros((self.n,)),Q)
 
         # Nonlinear model
-        # Discrete Volterra-Lotka predator-prey model
-        new_x = np.array([0.1*x[0]*(1-x[0]) - 0.7*x[0]*x[1], 0.1*x[0]*x[1]])
-        return new_x
+        # Discrete predator-prey model
+        # https://www.researchgate.net/publication/335681339_Dynamics_of_a_discrete_nonlinear_prey-predator_model
+        # new_x = np.array([0.2*x[0]*(1-x[0]) - 0.43*x[0]*x[1] + u[0], 3*x[0]*x[1] + u[1]])
+
+        # Nonlinear model
+        # Euler forward discretized Volterra lotka
+        alpha = 0.5
+        beta = 0.2
+        delta = 0.6
+        gamma = 0.3
+        x1 = x[0] + self.Ts*(alpha*x[0] - beta*x[0]*x[1] + u[0])
+        x2 = x[1] + self.Ts*(delta*x[0]*x[1] - gamma*x[1] + u[1])
+        return np.array([x1,x2])
 
     def measurement_model(self, x):
         """
@@ -41,7 +65,7 @@ class Simulator:
         """
 
         H = np.eye(self.o, self.n)
-        R = np.eye(self.o, self.o)
+        R = 0.0001*np.eye(self.o, self.o)
         return H@x + np.random.multivariate_normal(np.zeros((self.m,)),R)
 
     def step(self, u):
